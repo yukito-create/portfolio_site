53cd4763624c8a8168d365fd0b3b4d54f7dcc462
Yukito <193806322+yukito-create@users.noreply.github.com>
2026-07-01 12:30:12 +0900

    edited.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,45 @@
 'use strict';
 
+// スクロールスタック
+const sections = document.querySelectorAll("[data-section]");
+
+sections.forEach((section) => {
+  const inner = section.querySelector("[data-section-inner]");
+
+  const setFixed = () => {
+    gsap.set(inner, {
+      position: "fixed",
+      bottom: 0,
+    });
+  };
+
+  const setAbsolute = () => {
+    gsap.set(inner, {
+      position: "absolute",
+      bottom: "auto",
+    });
+  };
+
+  ScrollTrigger.create({
+    trigger: section,
+    start: "bottom bottom",
+    onEnter: setFixed,
+    onEnterBack: setFixed,
+    onLeave: setAbsolute,
+    onLeaveBack: setAbsolute,
+  });
+});
+
+
+
+
+
+
+
+
+
+
+// ハイブリッドスクロール
 const listWrapperEl = document.querySelector('.side-scroll-list-wrapper');
 const listEl = document.querySelector('.side-scroll-list');
 
