00e84793a3a69a4a7126fd3cbc19cac27429a989
yukito <test@mail.com>
2026-07-07 10:07:23 +0900

    edited.

--- js/main.js
+++ js/main.js
@@ -36,23 +36,6 @@ sections.forEach((section, index) => {
 });
 
 // ハイブリッドスクロール
-// const listWrapperEl = document.querySelector('.side-scroll-list-wrapper');
-// const listEl = document.querySelector('.side-scroll-list');
-
-// gsap.to(listEl, {
-//   x: () => -(listEl.clientWidth - listWrapperEl.clientWidth),
-//   ease: 'none',
-//   scrollTrigger: {
-//     trigger: listWrapperEl,
-//     start: 'top top',
-//     end: () => `+=${listEl.clientWidth - listWrapperEl.clientWidth}`,
-//     scrub: true,
-//     pin: true,
-//     anticipatePin: 1,
-//     invalidateOnRefresh: true,
-//   },
-// });
-
 const listWrapperEl = document.querySelector(".side-scroll-list-wrapper");
 const listEl = document.querySelector(".side-scroll-list");
 
